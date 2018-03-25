f44cc38954aef4cf0b3e8e923e7f246bc34a0ad0
ashblazer <11437681+ashblazer@users.noreply.github.com>
2018-03-25 23:57:55 +0700

    Midtrans Assignment done
    Took me a while, but the deed is done
    here is the test answers, you can run this file to get the automation

--- testcase2.py
+++ testcase2.py
@@ -16,18 +16,21 @@ with Browser('chrome') as browser:
      with browser.get_iframe('snap-midtrans') as iframe:
      	third = iframe.find_by_xpath("//a[@href='#/select-payment']")
      	third.first.click()
-     	time.sleep(3)
+     	#time.sleep(3)
      	fourth = iframe.find_link_by_href("#/credit-card").first.click()
-     	time.sleep(3)
+     	#time.sleep(3)
      	iframe.fill("cardnumber","4811111111111114")
      	iframe.find_by_xpath("//input[@placeholder='MM / YY']").fill("0123")
      	iframe.find_by_xpath("//input[@placeholder='123']").fill("123")
      	fifth = iframe.find_by_xpath("//a[@href='#/']").click()
      	time.sleep(3)
 
-     	with browser.get_iframe(2) as iframe2:
+     	with iframe.get_iframe(0) as iframe2:
      		iframe2.fill("PaRes","112233")
      		iframe2.find_by_name("ok").first.click()
-     		time.sleep(5)
-     		if iframe2.is_text_present('Transaction Successful'):
-     	        	print("Yes , Transaction SUCCESS")
+
+     with browser.get_iframe('snap-midtrans') as iframe3:
+     	if iframe3.is_text_present("Transaction successful",wait_time = 10):
+     		print("Yess, Transaction SUCCESS")
+     	else:
+     		print("Failed")
